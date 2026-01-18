973f11b929aab6090c46ef5e101a637c9cd6024f
maharmohibali9-afk <maharmohibali9@gmail.com>
2026-01-18 15:49:51 +0500

    Final Code Upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+   
+    const tablinks =  document.getElementsByClassName("tab-links");
+    const tabcontents =  document.getElementsByClassName("tab-contents");
+
+
+    function  opentab(tabname) {
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+                    
+                for(tabcontent of tabcontents){
+                    tabcontent.classList.remove("active-tab");
+                }
+
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+        
+    }
+
+
+var Sidemenu= document.getElementById("sidemenu");
+
+    function openmenu(){
+        Sidemenu.style.right = "0";
+    }
+
+
+    function openmenu(){
+        Sidemenu.style.right = "-200px";
+    }
+
+
+
+
+
+
+
+
+
+
+
+    
