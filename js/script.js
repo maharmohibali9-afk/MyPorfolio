347837353db14a291fb9f0ce0e05d6538f9c6947
maharmohibali9-afk <maharmohibali9@gmail.com>
2026-01-25 10:47:22 +0500

    Portfolio Project

--- js/script.js
+++ js/script.js
@@ -29,6 +29,26 @@ var Sidemenu= document.getElementById("sidemenu");
     }
 
 
+    const scriptURL  ="#";
+
+
+    const form = document.forms['submit-to-google-sheet'];
+    const msg = document.getElementById("msg");
+
+
+    form.addEventListener('submit', e =>{
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message Sent succesfully";
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.meassage))
+    })
+
 
 
 
